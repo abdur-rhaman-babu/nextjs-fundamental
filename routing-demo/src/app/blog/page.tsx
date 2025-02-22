e3607d3f75f56b39b53de1b08e96c61c1d4f56ab
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-02-22 14:46:46 +0600

    link

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 
 const Blog = () => {
     return (
         <div>
+            <Link href='/'>Home</Link>
             <h1>Blog</h1>
         </div>
     );
--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome Back!</h1>
+            <Link href='/blog'>Blog</Link>
+            <Link href='/docs'>Docs</Link>
+            <Link href='/login'>login</Link>
         </div>
     );
 };
